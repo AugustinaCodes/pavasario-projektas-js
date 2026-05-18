cdac510831b32e9baf47400ad5ee7c77d17b90af
Icema159 <aismantass@example.com>
2026-05-18 23:01:14 +0300

    PAV-6 add database connection

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const errorHandler = require("./middleware/errorHandler");
+const { testDatabaseConnection } = require("./config/db");
 
 const app = express();
 
@@ -15,11 +16,18 @@ app.use(
 app.use(express.json());
 app.use(cookieParser());
 
-app.get("/api/health", (req, res) => {
-  res.status(200).json({
-    status: "success",
-    message: "FitBook API is running",
-  });
+app.get("/api/health", async (req, res, next) => {
+  try {
+    await testDatabaseConnection();
+
+    res.status(200).json({
+      status: "success",
+      message: "FitBook API is running",
+      database: "connected",
+    });
+  } catch (error) {
+    next(error);
+  }
 });
 
 app.use(errorHandler);
